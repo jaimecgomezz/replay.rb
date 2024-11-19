5b2358420cc0b1e4d741a72f367e7ff057019b23
jaimecgomezz <jaimecgomezwork@gmail.com>
2024-11-18 20:52:09 -0600

    refactor: This it it, i guess

--- lib/replay.rb
+++ lib/replay.rb
@@ -1,117 +1,73 @@
 require 'pry'
+require 'httparty'
 
 require_relative 'replay/version'
 require_relative 'replay/sequence'
 require_relative 'replay/context'
 
 module Replay
-  def self.included(includer)
-    case includer
+  def self.included(scenario)
+    case scenario
     when Class
-      typerr!(includer)
+      raise(ArgumentError, "Replay should only be included in modules: #{klass}")
     when Module
-      includer.const_set(:REPLAY_ACTIONS, { initialization: ->(*) {} }) unless includer.const_defined?(:REPLAY_ACTIONS)
-      includer.const_set(:REPLAY_SEQUENCES, {}) unless includer.const_defined?(:REPLAY_SEQUENCES)
-      includer.const_set(:REPLAY_INCLUSIONS, []) unless includer.const_defined?(:REPLAY_INCLUSIONS)
-      includer.const_set(:REPLAY_EXTENSIONS, []) unless includer.const_defined?(:REPLAY_EXTENSIONS)
-      includer.extend(ReplayDefinitionMethods)
-      abstract_include(includer)
-    else
-      typerr!(includer)
+      scenario.const_set(:REPLAY_ACTIONS, {}) unless scenario.const_defined?(:REPLAY_ACTIONS)
+      scenario.const_set(:REPLAY_SEQUENCES, {}) unless scenario.const_defined?(:REPLAY_SEQUENCES)
+      scenario.const_set(:REPLAY_INCLUSIONS, []) unless scenario.const_defined?(:REPLAY_INCLUSIONS)
+      scenario.const_set(:REPLAY_EXTENSIONS, []) unless scenario.const_defined?(:REPLAY_EXTENSIONS)
+      scenario.const_set(:REPLAY_SCENARIO_NAME, scenario.name.split('::').last.gsub(/([A-Z]+)([A-Z][a-z])/, '\1_\2').gsub(/([a-z\d])([A-Z])/, '\1_\2').tr('-', '_').gsub(/\s/, '_').gsub(/__+/, '_').downcase) # rubocop:disable Layout/LineLength
+      scenario.extend(ReplayDefinitionMethods)
+      abstract_include(scenario)
     end
   end
 
-  def self.abstract_include(abstract_includer)
-    abstract_includer.extend(abstract_includer)
+  def self.abstract_include(scenario)
+    scenario.extend(scenario)
 
-    abstract_includer.module_eval do
-      abstract_includer_wrapper = Module.new do
-        define_method abstract_includer.__context_name do
-          abstract_includer.__context_instance
+    scenario.module_eval do
+      scenario_wrapper = Module.new do
+        define_method scenario::REPLAY_SCENARIO_NAME do
+          scenario.__replay_context
         end
       end
 
-      define_singleton_method(:included) do |concrete_includer|
-        concrete_includer.const_set(:REPLAY_INCLUSIONS, []) unless concrete_includer.const_defined?(:REPLAY_INCLUSIONS)
-        concrete_includer::REPLAY_INCLUSIONS << abstract_includer_wrapper
-        concrete_includer.send(:include, abstract_includer_wrapper)
+      define_singleton_method(:included) do |scenario_includer|
+        scenario_includer.const_set(:REPLAY_INCLUSIONS, []) unless scenario_includer.const_defined?(:REPLAY_INCLUSIONS)
+        scenario_includer::REPLAY_INCLUSIONS << scenario_wrapper
+        scenario_includer.send(:include, scenario_wrapper)
       end
 
-      define_singleton_method(:extended) do |concrete_extender|
-        concrete_extender.const_set(:REPLAY_EXTENSIONS, []) unless concrete_extender.const_defined?(:REPLAY_EXTENSIONS)
-        concrete_extender::REPLAY_EXTENSIONS << abstract_includer_wrapper
-        concrete_extender.send(:extend, abstract_includer_wrapper)
+      define_singleton_method(:extended) do |scenario_extender|
+        scenario_extender.const_set(:REPLAY_EXTENSIONS, []) unless scenario_extender.const_defined?(:REPLAY_EXTENSIONS)
+        scenario_extender::REPLAY_EXTENSIONS << scenario_wrapper
+        scenario_extender.send(:extend, scenario_wrapper)
       end
     end
-  end
-
-  def self.typerr!(klass)
-    raise(ArgumentError, "Replay should only be included in modules: #{klass}")
   end
 
   module ReplayDefinitionMethods
-    def on_start(default = nil, &blk)
-      raise(ArgumentError, "Hash must be provided to #on_start method: #{default}") unless default.is_a?(Hash)
-
-      self::REPLAY_ACTIONS[:initialization] = lambda do |argument = default|
-        argument = default if argument.nil? || argument.is_a?(Replay::Context)
-        raise(ArgumentError, "Hash must be provided to #on_start method: #{argument}") unless argument.is_a?(Hash)
-
-        instance_exec(argument, &blk)
-      end
+    def configuration(default = nil, &blk)
+      self::REPLAY_ACTIONS[:configure] = { default: default, blk: blk }
     end
 
     def action(name, default = nil, &blk)
-      self::REPLAY_ACTIONS[name.to_sym] = lambda do |argument = default|
-        argument = default if argument.nil? || argument.is_a?(Replay::Context)
-        instance_exec(argument, &blk)
-      end
+      self::REPLAY_ACTIONS[name.to_sym] = { default: default, blk: blk }
     end
 
     def sequence(name, defaults = nil, &blk)
-      if !defaults.nil? && !defaults.is_a?(Enumerable)
-        raise(ArgumentError, "An enumerble must be provided to '#{name}' sequence: #{defaults}")
-      end
-
       self::REPLAY_SEQUENCES[name.to_sym] = { defaults: defaults, blk: blk }
     end
 
-    def start(state = nil)
-      prompt = Pry::Prompt.new(
-        'replay',
-        'replay custom prompt',
-        [proc { "replay(#{__context_name})> " }]
-      )
-
-      Pry.start(__context_instance(state), prompt: prompt)
-    end
-
-    def __context_name
-      name
-        .split('::')
-        .last
-        .gsub(/([A-Z]+)([A-Z][a-z])/, '\1_\2')
-        .gsub(/([a-z\d])([A-Z])/, '\1_\2')
-        .tr('-', '_')
-        .gsub(/\s/, '_')
-        .gsub(/__+/, '_')
-        .downcase
-    end
-
-    def __context_instance(state = nil)
-      @__context_instance ||= Replay::Context.new(state, self::REPLAY_ACTIONS, self::REPLAY_SEQUENCES).tap do |context|
-        context.instance_exec(self::REPLAY_INCLUSIONS) do |subcontexts|
-          subcontexts.each do |subcontext|
-            singleton_class.include(subcontext)
-          end
-        end
-
-        context.instance_exec(self::REPLAY_EXTENSIONS) do |subcontexts|
-          subcontexts.each do |subcontext|
-            singleton_class.extend(subcontext)
-          end
-        end
+    def start(argument = nil)
+      context = Replay::Context.new(self).tap do |ctx|
+        ctx.configure(argument)
       end
+
+      Pry.start(context, prompt: Pry::Prompt.new('replay', 'replay custom prompt', [proc { "replay(#{self::REPLAY_SCENARIO_NAME})> " }]))
+    end
+
+    def __replay_context
+      @__replay_context ||= Replay::Context.new(self)
     end
   end
 end
--- lib/replay/context.rb
+++ lib/replay/context.rb
@@ -1,42 +1,118 @@
-require_relative 'context/sequencer'
-require_relative 'context/requester'
-
 module Replay
   class Context
-    include Sequencer
-    include Requester
+    def initialize(scenario)
+      @__replay_state = {}
 
-    def initialize(state = {}, actions = {}, sequences = {})
-      @__replay_state = state || {}
-      @__replay_sequences = sequences || {}
-
-      # Run user-provided initialization mechanism
-      initialization = actions.delete(:initialization)
+      # Define the configuration mechanism
+      __define_configure_method(
+        scenario,
+        scenario::REPLAY_ACTIONS.delete(:configure)
+      )
 
       # Dynamically define user-provided actions as instance methods
-      actions.each do |action, blk|
-        define_singleton_method(action) do |argument = nil, &handler|
-          argument = nil if argument.is_a?(Replay::Context)
-
-          result = instance_exec(argument, &blk)
-
-          return result if handler.nil?
-
-          handler.call(result)
-        rescue LocalJumpError => e
-          e.exit_value
-        end
+      scenario::REPLAY_ACTIONS.each do |name, info|
+        __define_action_method(scenario, name, info)
       end
 
-      state.nil? ? instance_eval(&initialization) : instance_exec(state, &initialization)
+      scenario::REPLAY_SEQUENCES.each do |name, info|
+        __define_sequence_method(scenario, name, info)
+      end
+
+      scenario::REPLAY_INCLUSIONS.each do |subcontext|
+        singleton_class.include(subcontext)
+      end
+
+      scenario::REPLAY_EXTENSIONS.each do |subcontext|
+        singleton_class.extend(subcontext)
+      end
     end
 
+    # State management methods
     def get(field)
       @__replay_state[field]
     end
 
     def set(field, value)
       @__replay_state[field] = value
+    end
+
+    # Request methods
+    def http(method, uri, options = {})
+      raise(ArgumentError, "Invalid HTTP method: #{method}") unless HTTParty.respond_to?(method)
+
+      # options = options.merge({ debug_output: $stdout })
+      response = HTTParty.send(method, uri, options)
+
+      return response unless block_given?
+
+      yield(response)
+    end
+
+    private
+
+    def __define_configure_method(scenario, info)
+      if info.nil?
+        @__replay_configured = true
+        define_singleton_method(:configure) {}
+        return
+      end
+
+      @__replay_configured = false
+      default, blk = info.values_at(:default, :blk)
+      _requireness, parameter = blk.parameters.first || []
+
+      define_singleton_method(:configure) do |argument = default, &handler|
+        argument = default if argument.nil?
+
+        raise(ArgumentError, "Expected '#{parameter}' on 'configure' action from #{scenario}, got: nil") if argument.nil? && !parameter.nil?
+
+        @__replay_configured = true
+
+        result = instance_exec(argument, &blk)
+
+        return result if handler.nil?
+
+        handler.call(result)
+      rescue LocalJumpError => e
+        e.exit_value
+      end
+    end
+
+    def __define_action_method(scenario, name, info)
+      default, blk = info.values_at(:default, :blk)
+      _requireness, parameter = blk.parameters.first || []
+
+      define_singleton_method(name) do |argument = default, &handler|
+        raise("#{scenario} hasn't been configured yet, call .configure") unless @__replay_configured
+
+        argument = default if argument.nil?
+
+        raise(ArgumentError, "Expected '#{parameter}' on '#{name}' action from #{scenario}, got nil") if argument.nil? && !parameter.nil?
+
+        result = instance_exec(argument, &blk)
+
+        return result if handler.nil?
+
+        handler.call(result)
+      rescue LocalJumpError => e
+        e.exit_value
+      end
+    end
+
+    def __define_sequence_method(scenario, name, info)
+      defaults, blk = info.values_at(:defaults, :blk)
+
+      define_singleton_method(name) do |items = defaults|
+        sequence = instance_variable_get("@#{name}")
+
+        return sequence unless sequence.nil?
+
+        items = defaults if items.nil?
+
+        raise(ArgumentError, "Expected an Iterable on '#{name}' sequence from #{scenario}, got: #{items}") unless items.is_a?(Iterable)
+
+        instance_variable_set("@#{name}", Replay::Sequence.new(self, name, items, blk))
+      end
     end
   end
 end
